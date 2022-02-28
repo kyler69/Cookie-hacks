9d32fe46dad3ac309df1488c4228400f4c27d564
kyler69 <99692199+kyler69@users.noreply.github.com>
2022-02-28 11:20:39 -0800

    Create Cookies.js
    /*
    NOTICE:
    Do NOT type a letter or a 0 before the select amount!
    */

---
+++ Cookies.js
@@ -0,0 +1,6 @@
+//inspect element
+var cookieAdd = prompt("Any positive number");
+Game.Earn(cookieAdd);
+
+//bookmarklet code
+javascript:(function()%7Bvar%20cookieAdd%20%3D%20prompt(%22Any%20positive%20number%22)%3B%0AGame.Earn(cookieAdd)%3B%7D)()%3B
